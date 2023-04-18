bc777aae42ee97769e44a3197cbc12efe2fd9c38
VictorBothSoares <127154736+VictorBothSoares@users.noreply.github.com>
2023-04-17 21:30:48 -0300

    Ajuste

--- mysql_connection.php
+++ mysql_connection.php
@@ -1,17 +1,17 @@
 <?php
-$conn = mysqli_connect('localhost', 'laboratorio', 'estudante', 'projeto_integrador');
+$conn = mysqli_connect('localhost', 'root', 'ienh', 'login');
 if (!$conn) {
     die('Could not connect: ' . mysqli_error());
 }
 echo 'Connected successfully';
 
-$sql = "SELECT usuario, senha FROM usuarios";
+$sql = 'SELECT login, senha FROM login';
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "\n" . "usuario: " . $row["usuario"]. " - Senha: " . $row["senha"];
+    echo "\n" . "usuario: " . $row["login"]. " - Senha: " . $row["senha"];
   }
 } else {
   echo "0 results";
